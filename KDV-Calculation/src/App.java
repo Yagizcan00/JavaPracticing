b02f85dc0f57fac132becdce03ddc7b129721023
Yağızcan Şahin <85365132+Yagizcan00@users.noreply.github.com>
2023-12-22 17:09:58 +0300

    Update

--- KDV-Calculation/src/App.java
+++ KDV-Calculation/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Define variables.
-        double price, kdvPrice;
+        double price, kdvPrice, lastKdvPrice;
 
         // Add Scanner class.
         try (Scanner inp = new Scanner(System.in)) {
@@ -15,9 +15,10 @@ public class App {
 
         // Calculate the new price.
         kdvPrice = price + (price * 0.18);
+        lastKdvPrice = kdvPrice > 1000 ? price + (price * 0.08) : price + (price * 0.18);
 
         // Show new price.
-        System.out.println("This is your new price with add KDV : " + kdvPrice);
+        System.out.println("This is your new price with add KDV : " + lastKdvPrice);
 
     }
 }
